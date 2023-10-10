884dbc71b60bd57912bae2db4b860bc5fd42427f
thanh quach <thanhb.quach@gmail.com>
2023-10-09 20:35:35 -0400

    data extraction testing

--- YT_Chat_Qdrant/main.py
+++ YT_Chat_Qdrant/main.py
@@ -23,23 +23,25 @@ client = QdrantClient(
 #     vectors_config=models.VectorParams(size=1536, distance=models.Distance.COSINE),
 # )
 
-url = "https://www.youtube.com/watch?v=2eWuYf-aZE4&t=5s"
-def get_chunks(youtube_url):
-    loader = YoutubeLoader.from_youtube_url(youtube_url)
-    transcript = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=100,
-        length_function=len
-    )
-    chunks = text_splitter.split_documents(transcript) # this will return a list of documents
-    return chunks
+# we only run this ONCE load and split the youtube transcript into chunks
+# url = "https://www.youtube.com/watch?v=2eWuYf-aZE4&t=5s"
+# def get_chunks(youtube_url):
+#     loader = YoutubeLoader.from_youtube_url(youtube_url)
+#     transcript = loader.load()
+#     text_splitter = RecursiveCharacterTextSplitter(
+#         chunk_size=1000,
+#         chunk_overlap=100,
+#         length_function=len
+#     )
+#     chunks = text_splitter.split_documents(transcript) # this will return a list of documents
+#     return chunks
 
 
 embeddings = OpenAIEmbeddings()
+# as the chunks are documents, we use from_documents method, we only run this ONCE to add the youtube transcript and create a vector store
+# Qdrant.from_documents(get_chunks(url), embeddings, url=os.getenv("QDRANT_URL"), api_key=os.getenv("QDRANT_API_KEY"), collection_name="first-collection")
 vector_store = Qdrant(collection_name="first-collection", embeddings=embeddings, client=client, distance_strategy="COSINE")
-# as the chunks are documents, we use from_documents method, we only run this ONCE to add the youtube transcript
-# create_vector_store = Qdrant.from_documents(get_chunks(url), embeddings, url=os.getenv("QDRANT_URL"), api_key=os.getenv("QDRANT_API_KEY"), collection_name="first-collection")
+
 
 #creating a chat feature
 qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0.7), chain_type="stuff", retriever=vector_store.as_retriever(),return_source_documents=True)
@@ -47,7 +49,7 @@ qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-3.5-turbo",tempe
 query = "what is stable diffusion?"
 # response = qa.run(query)
 response = qa({"query": query})
-print(response["result"])
+# print(response["result"])
 
 # we can take it one step further by feeding this result into another prompt to ask LLM to check if how good the answer is.
-# print(response["source_documents"])
+print(response["source_documents"])
--- webScraping/headless_browser.py
+++ webScraping/headless_browser.py
@@ -5,19 +5,71 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import json
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 
-url1="https://the-internet.herokuapp.com/login"
+url1="https://www.zolo.ca/"
 url2="https://the-internet.herokuapp.com/dynamic_loading/2"
 
-driver.get(url2)
-# driver.find_element(By.XPATH, '//*[@id="username"]').send_keys("tomsmith")
-# driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("SuperSecretPassword!")
+
+data = []
+
+# driver.get(url1)
+
+# profiles = driver.find_elements(By.CSS_SELECTOR, 'article.card-listing')
+
+# def run_loops(profiles):
+#     for count, profile in enumerate(profiles, start=1):
+#         try:
+#             price = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[1]/span[2]').text
+#         except:
+#             price = "N/A"
+#         bed = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[2]').text
+#         bath = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[3]').text
+#         try:
+#             area = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[4]').text
+#         except:
+#             area = "N/A"
+#         street = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/div[1]/a/h3/span[1]').text
+#         city = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/div[1]/a/h3/span[2]').text
+#         province = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/div[1]/a/h3/span[3]').text
+#         data.append({"address": f"{street} {city} {province}","price":price,"bed":bed,"bath":bath,"area":area}) 
+# driver.close()
+
+
+
+for page_number in range(1,10):
+    driver.get(f"https://www.zolo.ca/toronto-real-estate/page-{page_number}")
+    profiles = driver.find_elements(By.CSS_SELECTOR, 'article.card-listing')
+    for count, profile in enumerate(profiles, start=1):
+        try:
+            price = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[1]/span[2]').text
+        except:
+            price = "N/A"
+        bed = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[2]').text
+        bath = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[3]').text
+        try:
+            area = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/ul/li[4]').text
+        except:
+            area = "N/A"
+        street = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/div[1]/a/h3/span[1]').text
+        city = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/div[1]/a/h3/span[2]').text
+        province = profile.find_element(By.XPATH, f'//*[@id="gallery"]/div/article[{count}]/div[1]/div[1]/a/h3/span[3]').text
+        data.append({"address": f"{street} {city} {province}","price":price,"bed":bed,"bath":bath,"area":area})
+    driver.implicitly_wait(6) 
+driver.close()
+
+
+with open("data.json","w") as f:
+    json.dump(data,f)
+
+
+
+
 # driver.find_element(By.XPATH, '//*[@id="login"]/button').click()
-driver.find_element(By.XPATH, '//*[@id="start"]/button').click()
-driver.implicitly_wait(6)
-text = driver.find_element(By.XPATH, '//*[@id="finish"]/h4').text
-print(text)
+# driver.find_element(By.XPATH, '//*[@id="start"]/button').click()
+# driver.implicitly_wait(6)
+# text = driver.find_element(By.XPATH, '//*[@id="finish"]/h4').text
